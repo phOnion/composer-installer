2e5807e7f9ca9b3ab635b49c20c4d19ca19ebd58
Dimitar Dimitrov <daghostman.dimitrov@gmail.com>
2016-08-04 11:12:59 +0100

    Added `test` functionality to the install/update procedures

--- src/Composer/Installers/ModuleInstaller.php
+++ src/Composer/Installers/ModuleInstaller.php
@@ -10,10 +10,13 @@ use Composer\Package\PackageInterface;
 use Composer\Repository\InstalledRepositoryInterface;
 use Onion\Composer\Installers\Interfaces\InstallerInterface;
 use Onion\Composer\Installers\Traits\CommonInstallerTrait;
+use Onion\Composer\Installers\Traits\TestRunnerTrait;
 
 class ModuleInstaller extends LibraryInstaller implements InstallerInterface
 {
     use CommonInstallerTrait;
+    use TestRunnerTrait;
+
     public function supports($packageType):bool
     {
         return strtolower($packageType) === 'module';
@@ -34,18 +37,31 @@ class ModuleInstaller extends LibraryInstaller implements InstallerInterface
         }
 
         return 'modules/' .
-            $this->translatePackageNameToInstallPath(
-                $package->getPrettyName()
-            );
+        $this->translatePackageNameToInstallPath(
+            $package->getPrettyName()
+        );
     }
 
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
         parent::install($repo, $package);
+        try {
+            $this->runPackageTests($this->getInstallPath($package));
+        } catch (\RuntimeException $ex) {
+            $this->io->writeError($ex->getMessage());
+            $this->uninstall($repo, $package);
+        }
     }
 
     public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
     {
         parent::update($repo, $initial, $target);
+        try {
+            $this->runPackageTests($this->getInstallPath($target));
+        } catch (\RuntimeException $ex) {
+            $this->io->writeError($ex->getMessage());
+            $this->uninstall($repo, $target);
+            $this->install($repo, $initial);
+        }
     }
 }
